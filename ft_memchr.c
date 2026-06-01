eb22e1c3a5f327276272a3482227b55ff37becce
joaolive <devjoaolive@outlook.com>
2026-06-01 10:58:36 -0300

    refactor: ft_memchr function

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: joaolive <joaolive@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 15:05:06 by joaolive          #+#    #+#             */
-/*   Updated: 2025/08/07 11:37:30 by joaolive         ###   ########.fr       */
+/*   Updated: 2026/05/31 15:35:28 by joaolive         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,26 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	const unsigned char	*src;
+	const unsigned char	*p;
+	const size_t		*w;
+	size_t				x;
 
-	src = (const unsigned char *) s;
-	while (n--)
+	p = (const unsigned char *)s;
+	while (n && ((size_t)p & (sizeof(size_t) - 1)) != 0 && n--)
+			if (*p++ == (unsigned char)c)
+				return ((void *)(p - 1));
+	w = (const size_t *)p;
+	while (n >= sizeof(size_t))
 	{
-		if (*src == (unsigned char) c)
-			return ((void *) src);
-		src++;
+		x = *w ^ ((size_t)-1 / 0xFF * (unsigned char)c);
+		if (((x - (size_t)-1 / 0xFF) & ~x & ((size_t)-1 / 0xFF << 7)) != 0)
+			break ;
+		w++;
+		n -= sizeof(size_t);
 	}
+	p = (const unsigned char *)w;
+	while (n--)
+		if (*p == (unsigned char)c)
+			return ((void *)(p - 1));
 	return (NULL);
 }
