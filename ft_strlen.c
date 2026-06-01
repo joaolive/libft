a559f9d5cbd3d292dd44dab84e5e1641111612a0
joaolive <devjoaolive@outlook.com>
2026-06-01 10:59:27 -0300

    refactor: ft_strlen function

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: joaolive <joaolive@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 15:05:53 by joaolive          #+#    #+#             */
-/*   Updated: 2025/07/09 14:26:47 by joaolive         ###   ########.fr       */
+/*   Updated: 2026/05/31 00:12:06 by joaolive         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,21 @@
 
 size_t	ft_strlen(const char *s)
 {
-	const char	*p;
+	const size_t	*w;
+	const char		*p;
 
-	p = (const char *) s;
+	p = s;
+	while (((size_t)p & (sizeof(size_t) - 1)) != 0)
+	{
+		if (*p == '\0')
+			return ((size_t)(p - s));
+		p++;
+	}
+	w = (const size_t *)p;
+	while (((*w - ~(size_t)0 / 0xFF) & ~*w & (~(size_t)0 / 0xFF << 7)) == 0)
+		w++;
+	p = (const char *)w;
 	while (*p)
 		p++;
-	return (p - s);
+	return ((size_t)(p - s));
 }
