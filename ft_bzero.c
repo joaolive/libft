28ca051d6f92634569571b79933a43e8d09cc919
joaolive <devjoaolive@outlook.com>
2026-06-01 10:56:31 -0300

    refactor: ft_bzero function

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,14 +6,27 @@
 /*   By: joaolive <joaolive@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 16:37:55 by joaolive          #+#    #+#             */
-/*   Updated: 2025/08/07 11:23:05 by joaolive         ###   ########.fr       */
+/*   Updated: 2026/05/30 18:56:52 by joaolive         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bzero(void *s, size_t size)
+void	ft_bzero(void *s, size_t n)
 {
-	while (size--)
-		*(unsigned char *)s++ = '\0';
+	size_t			*word_ptr;
+	unsigned char	*byte_ptr;
+
+	byte_ptr = (unsigned char *)s;
+	while (n && ((size_t)byte_ptr & (sizeof(size_t) - 1)) != 0)
+	{
+		*byte_ptr++ = 0;
+		n--;
+	}
+	word_ptr = (size_t *)byte_ptr;
+	while (n >= sizeof(size_t) && (n -= sizeof(size_t), 1))
+		*word_ptr++ = 0;
+	byte_ptr = (unsigned char *)word_ptr;
+	while (n--)
+		*byte_ptr++ = 0;
 }
